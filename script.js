9b55b4b393842f50bf9d6df0af155da8837f1a31
Lucianne302 <68714080+Lucianne302@users.noreply.github.com>
2020-09-20 20:39:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,6 @@ function testCall() {
             data: {
                 client_id: catapiKey,
                 client_secret: cliSecret,
-                code: code,
                 grant_type: "client_credentials",
             },
 
